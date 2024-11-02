aa51525c65672c37779455986659ec9c7769578f
paradoxskin <1312269430@qq.com>
2024-11-02 14:06:50 +0800

    fix video's preview

--- scope.sh
+++ scope.sh
@@ -159,9 +159,9 @@ handle_image() {
         ## Video
         video/*)
             # Get embedded thumbnail
-            ffmpeg -i "${FILE_PATH}" -map 0:v -map -0:V -c copy "${IMAGE_CACHE_PATH}" && exit 6
+            #ffmpeg -i "${FILE_PATH}" -map 0:v -map -0:V -c copy "${IMAGE_CACHE_PATH}" && exit 6
             # Get frame 10% into video
-            #ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+            ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
             exit 1;;
 
         ## Audio
